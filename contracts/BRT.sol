2de38a65512de8ca4160975ae72ceec96d409a90
Agboola Feyikemi <54103882+Agboolafeyikemi@users.noreply.github.com>
2022-03-27 08:40:13 +0100

    interest

--- contracts/BRT.sol
+++ contracts/BRT.sol
@@ -5,8 +5,9 @@ import "./IBored.sol";
 import "hardhat/console.sol";
 
 contract BoaredApe is ERC20 {
-    address private bored = 0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D;
-    IBRT private ape = IBRT(bored);
+    IBRT constant ape = IBRT(0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D);
+
+    uint256 constant INTERESTPERSECONDS = (86400 * 300);
 
     constructor(string memory _name, string memory _symbol)
         ERC20(_name, _symbol)
@@ -14,75 +15,67 @@ contract BoaredApe is ERC20 {
         _mint(msg.sender, 1000000 * 10**18);
     }
 
-    //0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d
     struct Stake {
         uint256 time;
-        address owner;
-        uint256 stakeAmount;
         bool valid;
+        uint256 stakeAmount;
+        address owner;
     }
     mapping(address => Stake) public stake;
     event StateChange(address from, uint256 _mount, uint256 time);
 
     function stakeBRT(uint256 amount) public returns (uint256 interest) {
         Stake storage s = stake[msg.sender];
-        require(ape.balanceOf(msg.sender) >= 1, "not a boredape holder");
+        // require(ape.balanceOf(msg.sender) >= 1, "not a boredape holder");
         transfer(address(this), amount);
         if (s.valid == true) {
-            uint256 daySpent = block.timestamp - s.time;
-            if (daySpent >= 3 days) {
-                interest = ((s.stakeAmount * (daySpent / 86400)) / 300);
+            uint256 secondsSpent = block.timestamp - s.time;
+            if (secondsSpent >= 259200) {
+                interest = (secondsSpent / INTERESTPERSECONDS) * s.stakeAmount;
                 s.stakeAmount += interest + amount;
             } else {
                 s.stakeAmount += amount;
             }
         } else {
-
             s.stakeAmount += amount;
-            s.staker = msg.sender;
+            s.owner = msg.sender;
             s.valid = true;
         }
-        
 
         s.time = block.timestamp;
         emit StateChange(msg.sender, amount, block.timestamp);
     }
 
-    function myStake() external view returns (Stake memory) {
-        return stakes[msg.sender];
-    }
-
-    function getStakeByAddress(address staker) external view returns (Stake memory) {
-        return stakes[staker];
-    }
-
-
-
-    function withdraw(uint256 amount) public {
+    function withdrawBRT(uint256 amount) public {
         Stake storage s = stake[msg.sender];
+
         require(s.valid == true, "you dont have money in the stake");
-        uint256 daySpent = block.timestamp - s.time;
-        if (daySpent >= 3 days) {
-            uint256 interest = ((s.stakeAmount * (daySpent / 86400)) / 300);
+        uint256 secondsSpent = block.timestamp - s.time;
+        if (secondsSpent >= 259200) {
+            uint256 interest = (secondsSpent / INTERESTPERSECONDS) *
+                s.stakeAmount;
             s.stakeAmount += interest;
         }
         require(s.stakeAmount >= amount, "insufficient funds");
         s.stakeAmount -= amount;
-        transfer(msg.sender, amount);
+        _transfer(address(this), msg.sender, amount);
         s.time = block.timestamp;
         s.stakeAmount == 0 ? s.valid = false : s.valid = true;
         emit StateChange(msg.sender, amount, block.timestamp);
     }
 
+    function myStake() external view returns (Stake memory) {
+        return stake[msg.sender];
+    }
+
     function calcReward()
         public
         view
-        returns (uint256 reward, uint256 daySpent)
+        returns (uint256 reward, uint256 secondsSpent)
     {
         Stake storage s = stake[msg.sender];
         require(s.valid == true, "you dont have money in the stake");
-        daySpent = block.timestamp - s.time;
-        reward = ((s.stakeAmount * (daySpent / 86400)) / 300);
+        secondsSpent = block.timestamp - s.time;
+        reward = (secondsSpent / INTERESTPERSECONDS) * s.stakeAmount;
     }
-
-    }
+}
